248c82479157ef827ea3f3bfafa198055eb93c40
aryan_mishra <aryanmishra1404@.com>
2024-03-09 14:22:17 +0530

    updated design and functionality

--- Clock/script.js
+++ Clock/script.js
@@ -3,8 +3,8 @@ const heading = document.querySelector("h1");
 
 const digiClock = document.querySelector(".digiClock");
 const digiHour = document.querySelector(".digiHour");
-const digiMinute= document.querySelector(".digiMinute");
-const digiSecond= document.querySelector(".digiSecond");
+const digiMinute = document.querySelector(".digiMinute");
+const digiSecond = document.querySelector(".digiSecond");
 // clock.addEventListener(onclick,()=>{
 // // clock.style.display = 'none';
 // // clock.style.display = "none";
@@ -12,75 +12,88 @@ const digiSecond= document.querySelector(".digiSecond");
 // console.log(style1);
 // })
 
-const showDigital = ()=>{
-    // clock.style.display ="block"
-    const date = new Date();
-    
-    // clock.innerHTML=
-//    const digiClock= document.createElement("div");
-//    digiClock.classList.add("digiClock");
-//    digiClock.style.display ="block";
-    // digiClock.innerHTML= `
-    //         <div class= "digiHour">
-    //         <h3>${date.getHours()}: </h3>
-    //         </div>
-            
-    //         <div class= "digiMinute">
-    //         <h3>${date.getMinutes()}: </h3>
-    //         </div>
-            
-    //         <div class= "digiSecond">
-    //         <h3>${date.getSeconds()} </h3>
-    //         </div>
-    //     `
+const showDigital = () => {
+  // clock.style.display ="block"
+  const date = new Date();
 
+  // clock.innerHTML=
+  //    const digiClock= document.createElement("div");
+  //    digiClock.classList.add("digiClock");
+  //    digiClock.style.display ="block";
+  // digiClock.innerHTML= `
+  //         <div class= "digiHour">
+  //         <h3>${date.getHours()}: </h3>
+  //         </div>
 
-    digiHour.innerHTML =`${date.getHours()}`;
-    digiMinute.innerHTML =`${date.getMinutes()}`;
-    digiSecond.innerHTML =`${date.getSeconds()}`;
+  //         <div class= "digiMinute">
+  //         <h3>${date.getMinutes()}: </h3>
+  //         </div>
 
-    setTimeout(showDigital,1000);
-    }
+  //         <div class= "digiSecond">
+  //         <h3>${date.getSeconds()} </h3>
+  //         </div>
+  //     `
 
-    
-function clickMe(){
-    // console.log("in ClickMe");
-    clock.style.display="none"
-    digiClock.style.visibility="visible";
-    heading.innerText=" My Digital Clock"
+  digiHour.innerHTML = `${date.getHours()}`;
+  digiMinute.innerHTML = `${date.getMinutes()}`;
+  digiSecond.innerHTML = `${date.getSeconds()}`;
+
+  setTimeout(showDigital, 1000);
+};
+
+function toggleClock() {
+  if (clock.style.display === "none") {
+    clock.style.display = "block";
+    digiClock.style.visibility = "hidden";
+    heading.innerText = " My Clock!";
+    document.getElementById("toggleClock").innerText = "Show Digital";
+  } else {
+    clock.style.display = "none";
+    digiClock.style.visibility = "visible";
+    heading.innerText = " My Digital Clock";
+    document.getElementById("toggleClock").innerText = "Hide Digital";
     showDigital();
+  }
+}
+function clickMe() {
+  // console.log("in ClickMe");
+  clock.style.display = "none";
+  digiClock.style.visibility = "visible";
+  heading.innerText = " My Digital Clock";
+  showDigital();
 }
 
-function digiClickMe(){
-    clock.style.display="block";
-    digiClock.style.visibility="hidden";
-    heading.innerText=" My Clock!"
+function digiClickMe() {
+  clock.style.display = "block";
+  digiClock.style.visibility = "hidden";
+  heading.innerText = " My Clock!";
 }
-clock.addEventListener("click",clickMe);
-digiClock.addEventListener("click",digiClickMe);
+// clock.addEventListener("click",clickMe);
+// digiClock.addEventListener("click",digiClickMe);
+document.getElementById("toggleClock").addEventListener("click", toggleClock);
 
 const hour = document.querySelector(".hour");
 const minute = document.querySelector(".minute");
 const second = document.querySelector(".second");
 
-function updateClock(){
-    const currDate = new Date();
-    // console.log(currDate);
-    const currHour = currDate.getHours();
+function updateClock() {
+  const currDate = new Date();
+  // console.log(currDate);
+  const currHour = currDate.getHours();
 
-    const currMinute = currDate.getMinutes();
-    const currSecond = currDate.getSeconds();
-    // console.log(currSecond);
+  const currMinute = currDate.getMinutes();
+  const currSecond = currDate.getSeconds();
+  // console.log(currSecond);
 
-    const hourRotation   = (currHour/12) *360;
-    
-    const minuteRotation = (currMinute/60) *360;
-    const secondRotation = (currSecond/60) *360;
-    // console.log(secondRotation);
+  const hourRotation = (currHour / 12) * 360;
 
-    hour.style.transform = `rotate(${hourRotation}deg)`;
-    minute.style.transform = `rotate(${minuteRotation}deg)`;
-    second.style.transform = `rotate(${secondRotation}deg)`;
+  const minuteRotation = (currMinute / 60) * 360;
+  const secondRotation = (currSecond / 60) * 360;
+  // console.log(secondRotation);
+
+  hour.style.transform = `rotate(${hourRotation}deg)`;
+  minute.style.transform = `rotate(${minuteRotation}deg)`;
+  second.style.transform = `rotate(${secondRotation}deg)`;
 }
 
-setInterval(updateClock,1000);
+setInterval(updateClock, 1000);
--- RangeSlider/script.js
+++ RangeSlider/script.js
@@ -1,81 +1,77 @@
-
-
-
 // const password =""
 const upper_set = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lower_set = 'abcdefghijklmnopqurstuvwxyz';
-const number_set = '1234567890';
-const symbol_set = '!@#$%^&*(){}?/';
+const lower_set = "abcdefghijklmnopqurstuvwxyz";
+const number_set = "1234567890";
+const symbol_set = "!@#$%^&*(){}?/";
 // console.log(symbol_set);
-const slider = document.querySelector('.slider');
-const value = document.getElementById('value');
+const slider = document.querySelector(".slider");
+const value = document.getElementById("value");
 
-const bg = slider.style.background.value ;
-console.log((bg))
+const bg = slider.style.background.value;
+console.log(bg);
 
-const pasBox = document.getElementById('pass-box');
+const pasBox = document.getElementById("pass-box");
 const totalChar = document.getElementById("total-char");
-console.log(totalChar)
-const upperInput = document.getElementById('upper-case');
-const lowerInput = document.getElementById('lower-case');
-const numberInput = document.getElementById('numbers');
-const symbolInput = document.getElementById('symbols');
+console.log(totalChar);
+const upperInput = document.getElementById("upper-case");
+const lowerInput = document.getElementById("lower-case");
+const numberInput = document.getElementById("numbers");
+const symbolInput = document.getElementById("symbols");
 
 value.innerHTML = slider.value;
 totalChar.value = slider.value;
 
-const getRandom = (dataset)=> {
+const getRandom = (dataset) => {
   // return 1;
-  return dataset[Math.floor(Math.random()*dataset.length)];
-}
+  return dataset[Math.floor(Math.random() * dataset.length)];
+};
 
-const getRandomData = function(dataset){
+const getRandomData = function (dataset) {
+  return dataset[Math.floor(Math.random() * dataset.length)];
+};
 
-    return dataset[Math.floor(Math.random()*dataset.length)]
-}
+const generatePassword = function (password = "") {
+  if (upperInput.checked) {
+    password += getRandomData(upper_set);
+  }
+  if (lowerInput.checked) {
+    password += getRandomData(lower_set);
+  }
+  if (numberInput.checked) {
+    password += getRandomData(number_set);
+  }
+  if (symbolInput.checked) {
+    password += getRandomData(symbol_set);
+  }
 
-const generatePassword = function(password=""){
-    if(upperInput.checked){
-        password += getRandomData(upper_set);
-    }
-    if(lowerInput.checked){
-        password += getRandomData(lower_set);
-    }
-    if(numberInput.checked){
-        password += getRandomData(number_set);
-    }
-    if(symbolInput.checked){
-        password += getRandomData(symbol_set);
-    }
-    
-    if(password.length< totalChar.value){
-        return generatePassword(password);
-    }
-    else{
-       return trimPassword(password,totalChar.value);
-    }
-    
-}
+  if (password.length < totalChar.value) {
+    return generatePassword(password);
+  } else {
+    return trimPassword(password, totalChar.value);
+  }
+};
 var pass = "";
 
-document.getElementById("btn").addEventListener('click',()=>{
-    pass = generatePassword();
-    pasBox.innerHTML = pass
-    // console.log(pass+" => "+ pass.length);
-})
-slider.oninput = function(){
-    totalChar.value= this.value;
-    value.innerHTML = this.value;
-    pass = generatePassword();
-    pasBox.innerHTML = pass
-}
+document.getElementById("btn").addEventListener("click", () => {
+  pass = generatePassword();
+  pasBox.innerHTML = pass;
+  //   totalChar.value = pass.length;
+  value.innerHTML = pass.length;
+  slider.value = pass.length;
+  //   console.log(pass + " => " + pass.length);
+});
+slider.oninput = function () {
+  totalChar.value = this.value;
+  value.innerHTML = this.value;
+  pass = generatePassword();
+  pasBox.innerHTML = pass;
+};
 
-const trimPassword = function(password, total){
-    if(password.length > total){
-        // console.log(password +", "+ password.length);
-        return password.substring(0,total);
-    }
-    else{
-        return password;
-    }
-}
+const trimPassword = function (password, total) {
+  if (password.length > total) {
+    // console.log(password +", "+ password.length);
+    return password.substring(0, total);
+  } else {
+    return password;
+  }
+};
